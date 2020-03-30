0a1d248cf2e0e026fff894ad70c5175732afccc7
Vitalij Vanyushin <vitalij.vanyushin@simbirsoft.com>
2020-03-30 16:32:16 +0400

    commit 8

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import Markdown from 'markdown-to-jsx';
+import AceEditor from 'react-ace';
+import brace from 'brace';
 import './App.css';
 
 const { ipcRenderer } = window.require('electron');
@@ -13,6 +15,15 @@ const App = () => {
 
   return (
     <div className="App">
+      <AceEditor
+        mode="markdown"
+        theme="dracula"
+        onChange={(newContent) => {
+          setLoadedFile(newContent);
+        }}
+        name="markdown_editor"
+        value={loadedFile}
+      />
       <Markdown>{loadedFile}</Markdown>
     </div>
   );
