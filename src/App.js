92e14053137897088ff5520d8aefe7958d1ab932
Vitalij Vanyushin <vitalij.vanyushin@simbirsoft.com>
2020-03-31 14:00:02 +0400

    commit 9

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import Markdown from 'markdown-to-jsx';
 import AceEditor from 'react-ace';
-import brace from 'brace';
+import styled from 'styled-components';
+import 'brace/mode/markdown';
+import 'brace/theme/dracula';
 import './App.css';
 
 const { ipcRenderer } = window.require('electron');
@@ -13,20 +15,62 @@ const App = () => {
     setLoadedFile(fileContent);
   });
 
+  ipcRenderer.setMaxListeners(0);
+
   return (
     <div className="App">
-      <AceEditor
-        mode="markdown"
-        theme="dracula"
-        onChange={(newContent) => {
-          setLoadedFile(newContent);
-        }}
-        name="markdown_editor"
-        value={loadedFile}
-      />
-      <Markdown>{loadedFile}</Markdown>
+      <Split>
+        <CodeWindow>
+          <AceEditor
+            mode="markdown"
+            theme="dracula"
+            onChange={(newContent) => {
+              setLoadedFile(newContent);
+            }}
+            name="markdown_editor"
+            value={loadedFile}
+          />
+        </CodeWindow>
+        <RenderedWindow>
+          <Markdown>{loadedFile}</Markdown>
+        </RenderedWindow>
+      </Split>
     </div>
   );
 };
 
 export default App;
+
+const Split = styled.div`
+  display: flex;
+  height: 100vh;
+`;
+
+const CodeWindow = styled.div`
+  flex: 1;
+  padding-top: 2rem;
+  background-color: #191324;
+`;
+
+const RenderedWindow = styled.div`
+  background-color: #191324;
+  width: 35%;
+  padding: 20px;
+  color: #fff;
+  border-left: 1px solid #302b3a;
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    color: #82d8d8;
+  }
+  h1 {
+    border-bottom: solid 3px #e54b4b;
+    padding-bottom: 10px;
+  }
+  a {
+    color: #e54b4b;
+  }
+`;
